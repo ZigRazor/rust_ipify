55641c7339bb5a9aa93ae6e7b080a3cc54336360
ZigRazor <zigrazor@gmail.com>
2021-07-09 09:01:08 +0000

    introduced Conversion for ipv4 to int

--- Rust_ipify_lib/src/ipify.rs
+++ Rust_ipify_lib/src/ipify.rs
@@ -12,6 +12,12 @@ pub fn get_ip4_string() -> Result<String, Box<dyn std::error::Error>> {
     Ok(body)
 }
 
+pub fn get_ip4_i64() -> Result<i64, Box<dyn std::error::Error>> {
+    let body = reqwest::blocking::get("https://api.ipify.org")?.text()?;
+    let result = ip_to_int(body);
+    Ok(result)
+}
+
 pub fn get_ip_json() -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
     let body = reqwest::blocking::get("https://api.ipify.org?format=json")?
         .json::<HashMap<String, String>>()?;
@@ -21,4 +27,18 @@ pub fn get_ip_json() -> Result<HashMap<String, String>, Box<dyn std::error::Erro
 pub fn get_ip_string() -> Result<String, Box<dyn std::error::Error>> {
     let body = reqwest::blocking::get("https://api.ipify.org")?.text()?;
     Ok(body)
+}
+
+fn ip_to_int(ip :String) -> i64 {
+    let mut result : i64 = 0;
+    let splitted_ip : Vec<&str> = ip.split('.').collect();
+    if splitted_ip.len() == 4 {
+        let first_block : i64 = splitted_ip[0].parse::<i64>().unwrap();
+        let second_block : i64 = splitted_ip[1].parse::<i64>().unwrap();
+        let third_block : i64 = splitted_ip[2].parse::<i64>().unwrap();
+        let fourth_block : i64 = splitted_ip[3].parse::<i64>().unwrap();       
+        
+        result = (first_block<<24)+(second_block<<16)+(third_block<<8)+(fourth_block<<0);
+    }
+    result
 }
--- Rust_ipify_lib_test/src/main.rs
+++ Rust_ipify_lib_test/src/main.rs
@@ -1,8 +1,9 @@
-extern crate ipify_lib;
-use ipify_lib::ipify::get_ip_json;
-use ipify_lib::ipify::get_ip_string;
-use ipify_lib::ipify::get_ip4_json;
-use ipify_lib::ipify::get_ip4_string;
+extern crate rust_ipify;
+use rust_ipify::ipify::get_ip_json;
+use rust_ipify::ipify::get_ip_string;
+use rust_ipify::ipify::get_ip4_json;
+use rust_ipify::ipify::get_ip4_string;
+use rust_ipify::ipify::get_ip4_i64;
 fn main() {
     println!("inside main of test ");
 
@@ -31,4 +32,12 @@ fn main() {
         Ok(r) => println!("Result IP v4 ( String ): \n\t{:#?}\n", r),
         Err(e) => println!("error in request {:?}", e),
     }
+
+    let result_ip4_i64 = get_ip4_i64();
+    match result_ip4_i64 {
+        Ok(r) => println!("Result IP v4 ( i64 ): \n\t{:#?}\n", r),
+        Err(e) => println!("error in request {:?}", e),
+    }
+
+
 }
